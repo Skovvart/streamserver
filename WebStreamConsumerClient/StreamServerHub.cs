c7db41cedf5aa3d6343edd543bd7d7d997028534
Nicolai Skovvart <skovvart@gmail.com>
2019-09-23 23:14:38 +0200

    Add web client

---
+++ WebStreamConsumerClient/StreamServerHub.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.SignalR;
+using StreamServer;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace WebStreamConsumerClient
+{
+    public class StreamServerHub : Hub
+    {
+        public async IAsyncEnumerable<StreamReply> BeginStream(string name, CancellationToken cts) {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new Streamer.StreamerClient(channel);
+            var replyStream = client.GetReplyStream(new StreamContextRequest { Name = name }, cancellationToken: cts);
+            await foreach (var streamReply in replyStream.ResponseStream.ReadAllAsync(cts))
+                yield return streamReply;
+        }
+    }
+}
---
+++ WebStreamConsumerClient/wwwroot/js/stream.js
@@ -0,0 +1,31 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/stream").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    connection.stream("BeginStream", user).subscribe({
+        next: (item) => {
+            var li = document.createElement("li");
+            li.textContent = item.message;
+            document.getElementById("messagesList").appendChild(li);
+        },
+        error: (err) => {
+            var li = document.createElement("li");
+            li.textContent = err;
+            document.getElementById("messagesList").appendChild(li);
+        }
+    });
+
+    event.preventDefault();
+}
+);
